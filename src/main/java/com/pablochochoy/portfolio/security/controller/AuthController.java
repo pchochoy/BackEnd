ce5dd70bb755b5c673cd66d378f028f42703ab93
unknown <pchochoy@afascl.coop>
2022-10-19 21:53:37 -0300

    make it better

--- src/main/java/com/pablochochoy/portfolio/security/controller/AuthController.java
+++ src/main/java/com/pablochochoy/portfolio/security/controller/AuthController.java
@@ -32,7 +32,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 //@CrossOrigin
 //@CrossOrigin(origins="http://localhost:4200/")
-@CrossOrigin(origins="https://pdchportfolio.web.app/porfolio/")
+//@CrossOrigin(origins="https://pdchportfolio.web.app/porfolio/")
+@CrossOrigin(origins="https://pdchportfolio.web.app/")
+
 public class AuthController {
     
     @Autowired
